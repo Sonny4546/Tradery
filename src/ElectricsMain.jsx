93ad4b1ba0d8bb044c014818349f365951e5ed2e
Saiki <76763287+Sonny4546@users.noreply.github.com>
2024-11-08 09:55:22 +0000

    proper undo

--- src/ElectricsMain.jsx
+++ src/ElectricsMain.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'index.css'
-import App from 'Matterhorn.jsx'
+import './main.css'
+import App from './Matterhorn.jsx'
 
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
