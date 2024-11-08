8c9fdac870adf35be836646f1117fc2ef3a76e79
Saiki <76763287+Sonny4546@users.noreply.github.com>
2024-11-08 10:02:05 +0000

    redo

--- src/Matterhorn.jsx
+++ src/Matterhorn.jsx
@@ -8,6 +8,7 @@ let page = App
 function App() {
   return (
     <>
+    <body style="background-image: url(images/900px.png);background-size: cover;">
       <h3 class="user">User Login</h3>
       <div class="email_input">
       <label>Enter Email Address</label>
@@ -19,6 +20,7 @@ function App() {
       </div>
       <GoogleLogin onSuccess={credentialResponse => {console.log(credentialResponse); login + 1;}}
       onError={() => {console.log('Login Failed');}}/>
+    </body>
     </>
   )
 }
