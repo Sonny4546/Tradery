7b172577bdfb12186829f61b098262b1c23e2a45
Saiki <76763287+Sonny4546@users.noreply.github.com>
2024-11-09 11:06:23 +0000

    Login Page plsss
    Co-authored-by: Lynx <LynxLenior@users.noreply.github.com>

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -5,20 +5,18 @@ import { GoogleLogin } from '@react-oauth/google';
 export default function LoginPage() {
   return (
     <>
-    <div class="LogContain">
-    <div class="Box"> 
-      <div class="Logo"></div>
-      <h2 class="user">USER LOGIN</h2>
-        <input class="email" type="text" placeholder="Email Address"></input>
-      </div>
-        <input class="pass" type="text" placeholder="User Password"></input>
-      </div>
-      <a href="#" className="forgot-password">Forgot Password?</a>
+    <div class="circle"></div>
+    <div class="square">
       <div class="google-button">
         <GoogleLogin onSuccess={credentialResponse => {console.log(credentialResponse);}}
         onError={() => {console.log('Login Failed');}}/>
-      <a href="#" className="create-account">Create New Account</a>
       </div>
+        <input class="emBox" type="text" placeholder="Email"></input>
+        <input class="psBox" type="text" placeholder="Password"></input>
+      <p> USER LOGIN </p>
+        <a href="#">Forgot Password?</a>
+            <a class="cAcc" href="#">Create Account</a>
+    </div>
     </>
   )
 }
