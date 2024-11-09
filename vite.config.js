e3db4ce7f2a7e248e2d5a5688d7cbd9b7b825cc4
Saiki <76763287+Sonny4546@users.noreply.github.com>
2024-11-09 03:39:12 +0000

    router update 5

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Tradery/"
-})
+  base: "/Tradery/",
+  build: {
+    rollupOptions: {
+      external: ["react", "react-router", "react-router-dom", "react-redux"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
+});
