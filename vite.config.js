f94f19ecf5c6ecade721f9f5d4def0427254e5eb
Saiki <76763287+Sonny4546@users.noreply.github.com>
2024-11-09 04:21:53 +0000

    undo react fix

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Tradery/",
-  resolve: {
-    alias: {
-      'react': 'https://cdn.skypack.dev/react@17',
-      'react-dom': 'https://cdn.skypack.dev/react-dom@17'
-    }
-  },
+  base: "/Tradery/"
 });
 
