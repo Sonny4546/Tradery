e7f58236d83a86ea0636bcb911623f6f958c0fb3
Saiki <76763287+Sonny4546@users.noreply.github.com>
2024-11-09 05:15:46 +0000

    navbar fix

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -3,41 +3,43 @@ import '../src/main.css'
 export default function HomePage() {
     return (
       <>
-      <nav class="menu-container">
-              <input type="checkbox" aria-label="Toggle menu" />
-              <span></span>
-              <span></span>
-              <span></span>
-              
-              <div class="menu">
-              <ul>
-                  <li>
-                  <a href="#home">
-                      Tradery
-                  </a>
-                  </li>
-              </ul>
-              <ul>
-                  <li>
-                  <a href="#signup">
-                      Messages
-                  </a>
-                  </li>
-                  <li>
-                  <a href="#login">
-                      User
-                  </a>
-                  </li>
-              </ul>
-              </div>
-          </nav>
-          <div class="Main" id="wrapper">
-              <form>
-                  <i class="fa fa-search">
-                  </i>
-                  <input name="keyword" placeholder="Search for Items..."></input>
-                  <button type="submit">Search</button>
-              </form>
+      <div class="body">
+            <nav class="menu-container">
+                <input type="checkbox" aria-label="Toggle menu" />
+                <span></span>
+                <span></span>
+                <span></span>
+                
+                <div class="menu">
+                <ul>
+                    <li>
+                    <a href="#home">
+                        Tradery
+                    </a>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                    <a href="#/messages">
+                        Messages
+                    </a>
+                    </li>
+                    <li>
+                    <a href="#/login">
+                        User
+                    </a>
+                    </li>
+                </ul>
+                </div>
+            </nav>
+            <div class="Main" id="wrapper">
+                <form>
+                    <i class="fa fa-search">
+                    </i>
+                    <input name="keyword" placeholder="Search for Items..."></input>
+                    <button type="submit">Search</button>
+                </form>
+            </div>
           </div>
       </>
     )
