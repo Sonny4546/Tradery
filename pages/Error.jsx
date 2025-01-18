5bcc4997bb6a722b9e9b0de586a9c98cdf75f585
Saiki <76763287+Sonny4546@users.noreply.github.com>
2025-01-18 06:55:39 +0000

    error page update 4

--- pages/Error.jsx
+++ pages/Error.jsx
@@ -5,13 +5,21 @@ export default function ErrorPage() {
   console.error(error);
 
   return (
-    <div id="error-page" class="error">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-      <a href="#" class="myButton">Go to Homepage</a>
+    <div id="error-page">
+      <a href="#">
+      <svg height="0.8em" width="0.8em" viewBox="0 0 2 1" preserveAspectRatio="none">
+      <polyline
+            fill="none" 
+            stroke="#777777" 
+            stroke-width="0.1" 
+            points="0.9,0.1 0.1,0.5 0.9,0.9" 
+      />
+      </svg> Home
+      </a>
+      <div class="background-wrapper">
+        <h1 id="visual">Error!</h1>
+      </div>
+      <p>The page you’re looking for does not exist.</p>
     </div>
   );
 }
