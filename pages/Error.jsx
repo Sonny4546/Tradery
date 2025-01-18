32dd4864348c469e4ba40396bd5c22a252268cd7
Saiki <76763287+Sonny4546@users.noreply.github.com>
2025-01-18 08:43:05 +0000

    error page update 8

--- pages/Error.jsx
+++ pages/Error.jsx
@@ -7,20 +7,20 @@ export default function ErrorPage() {
   return (
     <div id="error-page">
       <div class="error">
-        <a href="#">
-        <svg height="0.8em" width="0.8em" viewBox="0 0 2 1" preserveAspectRatio="none">
-        <polyline
-              fill="none" 
-              stroke="#777777" 
-              stroke-width="0.1" 
-              points="0.9,0.1 0.1,0.5 0.9,0.9" 
-        />
-        </svg> Home
+        <a class="errorButton" href="/home">
+          <svg height="0.8em" width="0.8em" viewBox="0 0 2 1" preserveAspectRatio="none">
+          <polyline
+                fill="none" 
+                stroke="#777777" 
+                stroke-width="0.1" 
+                points="0.9,0.1 0.1,0.5 0.9,0.9" 
+          />
+          </svg> Home
         </a>
         <div class="background-wrapper">
           <h1 id="visual">Error!</h1>
         </div>
-        <p>The page you’re looking for does not exist.</p>
+        <p class="errorText">The page you’re looking for does not exist.</p>
         <p>
           <i>{error.statusText || error.message}</i>
         </p>
