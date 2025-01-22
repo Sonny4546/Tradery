017a9637c659793d034263763077da99d7a5a873
Saiki <76763287+Sonny4546@users.noreply.github.com>
2025-01-22 14:45:28 +0000

    db fix again part 2

--- pages/UserDB.jsx
+++ pages/UserDB.jsx
@@ -1,7 +1,8 @@
 import '../src/main.css'
 import React, { useState, useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
-import {Outlet} from "react-router-dom";
+import { Outlet } from 'react-router-dom'
+import { Navbar } from 'react-bootstrap'
 
 export default function UserDB() {
     return (
