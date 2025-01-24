ae3eac5ecc02b8608c0ebb64f16838b0b886a747
Saiki <76763287+Sonny4546@users.noreply.github.com>
2025-01-24 03:50:12 +0000

    appwrite button oauth

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,7 +1,6 @@
 import '../src/main.css'
-import { Route, useNavigate } from 'react-router-dom';
-import { GoogleLogin } from '@react-oauth/google';
 import React, { useState, useEffect } from 'react'
+import { account, OAuthProvider } from './appwrite'
 
 // functions
 export const loginWithGoogle = async () => {
@@ -41,21 +40,24 @@ export default function LoginPage() {
         <GoogleLogin onSuccess={credentialResponse => {console.log(credentialResponse);
                 useNavigate("/Home");}}onError={() => {console.log('Login Failed');useNavigate("");}}/>
       </div> */}
-      <div id="g_id_onload"
-          data-client_id="1000212830777-k85n77vef4cbrma455a9ufpafl6sl316.apps.googleusercontent.com"
-          data-context="signin"
-          data-ux_mode="popup"
-          data-login_uri="https://cloud.appwrite.io/v1/account/sessions/oauth2/callback/google/678ba12f001dce105c6a"
-          data-auto_prompt="false">
-      </div>
-      <div class="g_id_signin"
-          data-type="standard"
-          data-shape="pill"
-          data-theme="outline"
-          data-text="signin_with"
-          data-size="large"
-          data-logo_alignment="left"
-          data-width="320">
+      <div class="google-button">
+        {/* <div id="g_id_onload"
+            data-client_id="1000212830777-k85n77vef4cbrma455a9ufpafl6sl316.apps.googleusercontent.com"
+            data-context="signin"
+            data-ux_mode="popup"
+            data-login_uri="https://cloud.appwrite.io/v1/account/sessions/oauth2/callback/google/678ba12f001dce105c6a"
+            data-auto_prompt="false">
+        </div>
+        <div class="g_id_signin"
+            data-type="standard"
+            data-shape="pill"
+            data-theme="outline"
+            data-text="signin_with"
+            data-size="large"
+            data-logo_alignment="left"
+            data-width="320">
+        </div> */}
+        <button onClick={loginWithGoogle}>Login with Google</button>
       </div>
       <p class="Use">By using this website, you confirm that you are a member of the University of the East and agree to use only a valid UE email address (@ue.edu.ph) for registration and access.</p>
       <p class="Proc"> By proceeding, you acknowledge and accept this condition.</p>
