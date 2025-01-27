bb3c0eed32840bcf0360e82e0d9e8e4937fbedd9
Saiki <76763287+Sonny4546@users.noreply.github.com>
2025-01-27 14:23:34 +0000

    test fix 19

--- src/ElectricsMain.jsx
+++ src/ElectricsMain.jsx
@@ -16,88 +16,8 @@ import Items from "../pages/DB-components/Items";
 import Requests from "../pages/DB-components/Requests";
 import { AuthProvider, useAuth } from "../pages/AuthHook";
 import { RouterProvider, HashRouter, createHashRouter, Route, Router, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
-import { Redirect } from "wouter";
 
-const PrivateRoutes = () => {
-  const session = useAuth();
-  if (!session) {
-    return (
-      <Redirect to="/" />
-    )
-  }
-}
-
-  // createRoutesFromElements(
-  //   <Route
-  //     path="/"
-  //     element={<LoginPage />}
-  //     errorElement={<ErrorPage />}>
-  //     {/* <Route element={<PrivateRoutes/>}> */}
-  //       <Route 
-  //         path="/Home" 
-  //         element={<HomePage/>} 
-  //         errorElement={<ErrorPage />} />
-  //     {/* </Route> */}
-  //   </Route>
-
-  // ),
-
-// export default function App() {
-//   return (
-//     <Routes>
-//         {/* <Route element={<PrivateRoutes/>}> */}
-//           <Route path="/Home" element={<HomePage/>} />
-//           <Route path="/Dashboard" element={<DBPage/>} />
-//           <Route path="/Dashboard/Profile" element={<Profile/>} />
-//           <Route path="/Dashboard/Messages" element={<Messages/>} />
-//           <Route path="/Dashboard/Post" element={<Post/>} />
-//           <Route path="/Dashboard/Items" element={<Items/>} />
-//           <Route path="/Dashboard/Requests" element={<Requests/>} />
-//         {/* </Route> */}
-//         <Route path="/" element={<LoginPage />} />
-//         <Route path="*" element={<ErrorPage />} />
-//     </Routes>
-//   );
-// };
-
-
-// {
-//   path: "/", element: <LoginPage />,
-//   errorElement: <ErrorPage />,
-// },
-// {
-//   path: "/Home", element: <HomePage />,
-//   errorElement: <ErrorPage />,
-// },
-// {
-//   path: "/Dashboard", element: <DBPage />,
-//   errorElement: <ErrorPage />,
-//   children:[
-//     {path: "/Dashboard/Profile", element: <Profile />},
-//     {path: "/Dashboard/Messages", element: <Messages />},
-//     {path: "/Dashboard/Post", element: <Post />},
-//     {path: "/Dashboard/Items", element: <Items />},
-//     {path: "/Dashboard/Requests", element: <Requests />},
-//   ]
-// },
-
-// const router = createHashRouter([
-//   <Router>
-//     <Route path="/" element={<LoginPage />} />
-//     <Route path="*" element={<ErrorPage />} />
-//       {/* <Route element={<PrivateRoutes/>}> */}
-//         <Route path="/Home" element={<HomePage/>} />
-//         <Route path="/Dashboard" element={<DBPage/>} />
-//         <Route path="/Dashboard/Profile" element={<Profile/>} />
-//         <Route path="/Dashboard/Messages" element={<Messages/>} />
-//         <Route path="/Dashboard/Post" element={<Post/>} />
-//         <Route path="/Dashboard/Items" element={<Items/>} />
-//         <Route path="/Dashboard/Requests" element={<Requests/>} />
-//       {/* </Route> */}
-//   </Router>
-// ]);
-
-const router = createHashRouter(
+const router = createHashRouter([
   {
     path: "/", element: <LoginPage />,
     errorElement: <ErrorPage />,
@@ -117,7 +37,7 @@ const router = createHashRouter(
       {path: "/Dashboard/Requests", element: <Requests />},
     ]
   },
-);
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
