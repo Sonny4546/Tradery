c683d806b9f52d405685808b240459bc72437cec
Saiki <76763287+Sonny4546@users.noreply.github.com>
2025-01-27 12:55:00 +0000

    test fix 6

--- src/ElectricsMain.jsx
+++ src/ElectricsMain.jsx
@@ -15,7 +15,7 @@ import Post from "../pages/DB-components/Post";
 import Items from "../pages/DB-components/Items";
 import Requests from "../pages/DB-components/Requests";
 import { AuthProvider, useAuth } from "../pages/AuthHook";
-import { RouterProvider, HashRouter } from "react-router-dom";
+import { RouterProvider, HashRouter, createHashRouter } from "react-router-dom";
 import { Redirect } from "wouter";
 
 const PrivateRoutes = () => {
