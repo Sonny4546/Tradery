53e73ee9c5da822ca547365333201dd9e5e8429b
Saiki <76763287+Sonny4546@users.noreply.github.com>
2025-01-27 13:01:36 +0000

    test fix

--- src/ElectricsMain.jsx
+++ src/ElectricsMain.jsx
@@ -15,7 +15,7 @@ import Post from "../pages/DB-components/Post";
 import Items from "../pages/DB-components/Items";
 import Requests from "../pages/DB-components/Requests";
 import { AuthProvider, useAuth } from "../pages/AuthHook";
-import { RouterProvider, HashRouter, createHashRouter } from "react-router-dom";
+import { RouterProvider, HashRouter, createHashRouter, Route } from "react-router-dom";
 import { Redirect } from "wouter";
 
 const PrivateRoutes = () => {
@@ -45,26 +45,41 @@ const PrivateRoutes = () => {
 //   );
 // };
 
+
+// {
+//   path: "/", element: <LoginPage />,
+//   errorElement: <ErrorPage />,
+// },
+// {
+//   path: "/Home", element: <HomePage />,
+//   errorElement: <ErrorPage />,
+// },
+// {
+//   path: "/Dashboard", element: <DBPage />,
+//   errorElement: <ErrorPage />,
+//   children:[
+//     {path: "/Dashboard/Profile", element: <Profile />},
+//     {path: "/Dashboard/Messages", element: <Messages />},
+//     {path: "/Dashboard/Post", element: <Post />},
+//     {path: "/Dashboard/Items", element: <Items />},
+//     {path: "/Dashboard/Requests", element: <Requests />},
+//   ]
+// },
+
 const router = createHashRouter([
-  {
-    path: "/", element: <LoginPage />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/Home", element: <HomePage />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/Dashboard", element: <DBPage />,
-    errorElement: <ErrorPage />,
-    children:[
-      {path: "/Dashboard/Profile", element: <Profile />},
-      {path: "/Dashboard/Messages", element: <Messages />},
-      {path: "/Dashboard/Post", element: <Post />},
-      {path: "/Dashboard/Items", element: <Items />},
-      {path: "/Dashboard/Requests", element: <Requests />},
-    ]
-  },
+  <Route>
+      <Route element={<PrivateRoutes/>}>
+        <Route path="/Home" element={<HomePage/>} />
+        <Route path="/Dashboard" element={<DBPage/>} />
+        <Route path="/Dashboard/Profile" element={<Profile/>} />
+        <Route path="/Dashboard/Messages" element={<Messages/>} />
+        <Route path="/Dashboard/Post" element={<Post/>} />
+        <Route path="/Dashboard/Items" element={<Items/>} />
+        <Route path="/Dashboard/Requests" element={<Requests/>} />
+      </Route>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="*" element={<ErrorPage />} />
+  </Route>
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
