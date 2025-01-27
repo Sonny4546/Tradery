a30b2ff05b53b45e745b6d4b604ce97165ad4ca4
Saiki <76763287+Sonny4546@users.noreply.github.com>
2025-01-27 12:53:20 +0000

    test fix 5

--- src/ElectricsMain.jsx
+++ src/ElectricsMain.jsx
@@ -27,30 +27,50 @@ const PrivateRoutes = () => {
   }
 }
 
-export default function App() {
-  return (
-    <Routes>
-        {/* <Route element={<PrivateRoutes/>}> */}
-          <Route path="/Home" element={<HomePage/>} />
-          <Route path="/Dashboard" element={<DBPage/>} />
-          <Route path="/Dashboard/Profile" element={<Profile/>} />
-          <Route path="/Dashboard/Messages" element={<Messages/>} />
-          <Route path="/Dashboard/Post" element={<Post/>} />
-          <Route path="/Dashboard/Items" element={<Items/>} />
-          <Route path="/Dashboard/Requests" element={<Requests/>} />
-        {/* </Route> */}
-        <Route path="/" element={<LoginPage />} />
-        <Route path="*" element={<ErrorPage />} />
-    </Routes>
-  );
-};
+// export default function App() {
+//   return (
+//     <Routes>
+//         {/* <Route element={<PrivateRoutes/>}> */}
+//           <Route path="/Home" element={<HomePage/>} />
+//           <Route path="/Dashboard" element={<DBPage/>} />
+//           <Route path="/Dashboard/Profile" element={<Profile/>} />
+//           <Route path="/Dashboard/Messages" element={<Messages/>} />
+//           <Route path="/Dashboard/Post" element={<Post/>} />
+//           <Route path="/Dashboard/Items" element={<Items/>} />
+//           <Route path="/Dashboard/Requests" element={<Requests/>} />
+//         {/* </Route> */}
+//         <Route path="/" element={<LoginPage />} />
+//         <Route path="*" element={<ErrorPage />} />
+//     </Routes>
+//   );
+// };
+
+const router = createHashRouter([
+  {
+    path: "/", element: <LoginPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/Home", element: <HomePage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/Dashboard", element: <DBPage />,
+    errorElement: <ErrorPage />,
+    children:[
+      {path: "/Dashboard/Profile", element: <Profile />},
+      {path: "/Dashboard/Messages", element: <Messages />},
+      {path: "/Dashboard/Post", element: <Post />},
+      {path: "/Dashboard/Items", element: <Items />},
+      {path: "/Dashboard/Requests", element: <Requests />},
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-    <HashRouter>
-      <RouterProvider router={App} />
-    </HashRouter>
+      <RouterProvider router={router} />
     </AuthProvider>
   </React.StrictMode>,
 )
