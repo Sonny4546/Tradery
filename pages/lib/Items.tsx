a1955d05c36bd165e1b79c0a70784b567df21921
Saiki <76763287+Sonny4546@users.noreply.github.com>
2025-02-01 17:41:58 +0800

    updated items tsx

--- pages/lib/Items.tsx
+++ pages/lib/Items.tsx
@@ -3,9 +3,9 @@ import { database } from './appwrite';
 import { TraderyItems } from './ItemsInterface';
 
 export async function getItems() {
-    const {Alldocuments} = await database.listDocuments(import.meta.env.VITE_APPWRITE_DATABASE_ITEM_ID, import.meta.env.VITE_APPWRITE_COLLECTION_ITEM_ID);
+    const {documents} = await database.listDocuments(import.meta.env.VITE_APPWRITE_DATABASE_ITEM_ID, import.meta.env.VITE_APPWRITE_COLLECTION_ITEM_ID);
     return{
-        items:Alldocuments.map(document => {
+        items:documents.map(document => {
             const item: TraderyItems = {
                 $id: document.$id,
                 name: document.name,
