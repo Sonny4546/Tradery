dee1ed41ac76615b69afa543a14aa44b21a4ab0a
Lynx <148461348+LynxLenior@users.noreply.github.com>
2025-02-15 13:25:00 +0800

    Idk Changes

--- pages/Admin.tsx
+++ pages/Admin.tsx
@@ -7,35 +7,36 @@ import { Redirect } from 'wouter';
 export default function Register() {
   return (
     <div className="RegAdminContainer">
-  <div className="login-register-container">
-    <form>
-      <div className="form-field-wrapper">
-        <label>Name:</label>
-        <input required type="text" name="name" placeholder="Enter name..." />
-      </div>
+      <div className="login-register-container">
+        <form>
+          <div className="form-field-wrapper">
+            <label>Name:</label>
+            <input required type="text" name="name" placeholder="Enter name..."/>
+          </div>
 
-      <div className="form-field-wrapper">
-        <label>Email:</label>
-        <input required type="email" name="email" placeholder="Enter email..." />
-      </div>
+          <div className="form-field-wrapper">
+            <label>Email:</label>
+            <input required type="email" name="email" placeholder="Enter email..."/>
+          </div>
 
-      <div className="form-field-wrapper">
-        <label>Password:</label>
-        <input type="password" name="password1" placeholder="Enter password..." />
-      </div>
+          <div className="form-field-wrapper">
+            <label>Password:</label>
+            <input type="password" name="password1" placeholder="Enter password..."/>
+          </div>
 
-      <div className="form-field-wrapper">
-        <label>Confirm Password:</label>
-        <input type="password" name="password2" placeholder="Confirm password..." />
-      </div>
+          <div className="form-field-wrapper">
+            <label>Confirm Password:</label>
+            <input type="password" name="password2" placeholder="Confirm password..."/>
+          </div>
 
-      <div className="form-field-wrapper">
-        <input type="submit" value="Register" className="btn" />
-      </div>
-    </form>
+          <div className="form-field-wrapper">
+            <input type="submit" value="Register" class="btn"/>
+          </div>
 
-    <p>Already have an account? <a href="/login">Login</a></p>
+        </form>
+
+      <p>Already have an account?</p>
+    </div>
   </div>
-</div>
   )
 }
